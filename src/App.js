34b7e1d1dfe3c65ab84089ebd473d9dbbec0e449
Caterina Paun <caterina.paun@gmail.com>
2021-05-25 21:57:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useCallback } from 'react';
+import ReactPlayer from 'react-player';
 
-function App() {
+const key = process.env.REACT_APP_API_KEY;
+
+export default function App() {
+  const [fact, setFact] = useState();
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+
+  let getFacts = useCallback(async () => {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setFact(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getFacts();
+  }, [getFacts]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {fact && (
+        <>
+          <h1>{fact.title}</h1>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url={fact.url}
+              width='100%'
+              height='100%'
+            />
+          </div>
+          <p className='description'>{fact.explanation}</p>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
