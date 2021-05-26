0ca2d606e7006617eba4973fc185fe87763ff093
Caterina Paun <caterina.paun@gmail.com>
2021-05-26 13:29:35 -0700

    Update: check for image vs video

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const key = process.env.REACT_APP_API_KEY;
 
 export default function App() {
   const [fact, setFact] = useState();
+  const [isImage, setIsImage] = useState(false);
+  const [isVideo, setIsVideo] = useState(false);
 
   const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
 
@@ -14,10 +16,12 @@ export default function App() {
       let response = await fetch(url);
       let data = await response.json();
       setFact(data);
+      if (data.media_type === 'image') setIsImage(true);
+      if (data.url.includes('youtube')) setIsVideo(true);
     } catch (error) {
       console.error(error);
     }
-  }, []);
+  });
 
   useEffect(() => {
     getFacts();
@@ -26,18 +30,25 @@ export default function App() {
   return (
     <div className='App'>
       {fact && (
-        <>
+        <main>
           <h1>{fact.title}</h1>
-          <div className='player-wrapper'>
-            <ReactPlayer
-              className='react-player'
-              url={fact.url}
-              width='100%'
-              height='100%'
-            />
-          </div>
+          {isImage && (
+            <figure className='figure'>
+              <img src={fact.url} alt={fact.title} />
+            </figure>
+          )}
+          {isVideo && (
+            <div className='player-wrapper'>
+              <ReactPlayer
+                className='react-player'
+                url={fact.url}
+                width='100%'
+                height='100%'
+              />
+            </div>
+          )}
           <p className='description'>{fact.explanation}</p>
-        </>
+        </main>
       )}
     </div>
   );
