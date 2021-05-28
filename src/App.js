5f274b85835815d38610d7893dd656104ab7f19f
Caterina Paun <caterina.paun@gmail.com>
2021-05-28 16:01:02 -0700

    Update: main component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,11 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player';
 
-const key = process.env.REACT_APP_API_KEY;
+import APOD from './components/APOD';
 
 export default function App() {
-  const [fact, setFact] = useState();
-  const [isImage, setIsImage] = useState(false);
-  const [isVideo, setIsVideo] = useState(false);
-
-  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
-
-  async function getFacts() {
-    try {
-      let response = await fetch(url);
-      let data = await response.json();
-      setFact(data);
-      if (data.media_type === 'image') setIsImage(true);
-      if (data.url.includes('youtube')) setIsVideo(true);
-    } catch (error) {
-      console.error('This is the error', error);
-    }
-  }
-
-  useEffect(() => {
-    getFacts();
-  }, []);
-
   return (
-    <div className='App'>
-      {fact && (
-        <main>
-          <h1>{fact.title}</h1>
-          <section className='content'>
-            {isImage && (
-              <figure className='figure'>
-                <img src={fact.url} alt={fact.title} />
-              </figure>
-            )}
-            {isVideo && (
-              <div className='player-wrapper'>
-                <ReactPlayer
-                  className='react-player'
-                  url={fact.url}
-                  width='100%'
-                  height='100%'
-                />
-              </div>
-            )}
-            <p className='description'>{fact.explanation}</p>
-          </section>
-        </main>
-      )}
-    </div>
+    <main className='App'>
+      <APOD></APOD>
+    </main>
   );
 }
---
+++ src/components/APOD.js
@@ -0,0 +1,65 @@
+import './APOD.css';
+
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+
+const key = process.env.REACT_APP_APOD_API_KEY;
+const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+
+export default function APOD() {
+  const [fact, setFact] = useState();
+  const [isImage, setIsImage] = useState(false);
+  const [isVideo, setIsVideo] = useState(false);
+  const [error, setError] = useState();
+
+  async function getFacts() {
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      setFact(data);
+      if (data.media_type === 'image') setIsImage(true);
+      if (data.url.includes('youtube')) setIsVideo(true);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    getFacts();
+  }, []);
+
+  return (
+    <>
+      {error && <div>Error: {error.message}</div>}
+      {fact && (
+        <>
+          <h1>NASA Astronomy Picture of the Day</h1>
+          <section className='content'>
+            {isImage && (
+              <figure className='figure'>
+                <img src={fact.url} alt={fact.title} />
+              </figure>
+            )}
+            {isVideo && (
+              <div className='player-wrapper'>
+                <ReactPlayer
+                  className='react-player'
+                  url={fact.url}
+                  width='100%'
+                  height='100%'
+                />
+              </div>
+            )}
+            <section className='description'>
+              <h2>{fact.title}</h2>
+              <small>{new Date(fact.date).toDateString()}</small>
+              <p className='explanation'>{fact.explanation}</p>
+              <small>Copyright: {fact.copyright}</small>
+            </section>
+          </section>
+        </>
+      )}
+    </>
+  );
+}
