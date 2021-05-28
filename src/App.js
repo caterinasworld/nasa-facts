bf71a9021d75040df76bf426ec4d507952347b5b
Caterina Paun <caterina.paun@gmail.com>
2021-05-27 22:34:36 -0700

    Update: app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
 
 const key = process.env.REACT_APP_API_KEY;
@@ -11,7 +11,7 @@ export default function App() {
 
   const url = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
 
-  let getFacts = useCallback(async () => {
+  async function getFacts() {
     try {
       let response = await fetch(url);
       let data = await response.json();
@@ -19,35 +19,37 @@ export default function App() {
       if (data.media_type === 'image') setIsImage(true);
       if (data.url.includes('youtube')) setIsVideo(true);
     } catch (error) {
-      console.error(error);
+      console.error('This is the error', error);
     }
-  });
+  }
 
   useEffect(() => {
     getFacts();
-  }, [getFacts]);
+  }, []);
 
   return (
     <div className='App'>
       {fact && (
         <main>
           <h1>{fact.title}</h1>
-          {isImage && (
-            <figure className='figure'>
-              <img src={fact.url} alt={fact.title} />
-            </figure>
-          )}
-          {isVideo && (
-            <div className='player-wrapper'>
-              <ReactPlayer
-                className='react-player'
-                url={fact.url}
-                width='100%'
-                height='100%'
-              />
-            </div>
-          )}
-          <p className='description'>{fact.explanation}</p>
+          <section className='content'>
+            {isImage && (
+              <figure className='figure'>
+                <img src={fact.url} alt={fact.title} />
+              </figure>
+            )}
+            {isVideo && (
+              <div className='player-wrapper'>
+                <ReactPlayer
+                  className='react-player'
+                  url={fact.url}
+                  width='100%'
+                  height='100%'
+                />
+              </div>
+            )}
+            <p className='description'>{fact.explanation}</p>
+          </section>
         </main>
       )}
     </div>
